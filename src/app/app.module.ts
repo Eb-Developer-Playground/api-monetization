5ad40093a8d58030c42cb50e552cbe95b7880636
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-09 09:56:19 +0300

    navbar component and design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from '../material.module';
+import { NavbarComponent } from './layout/components/navbar/navbar.component';
+import { CatalogSectionComponent } from './layout/components/catalog-section/catalog-section.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, CatalogSectionComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
