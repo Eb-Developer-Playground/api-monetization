4a84a842f0e12d689398217ff2d48d26ef21fd12
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-11 14:26:33 +0300

    route components in layout module through the base component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,9 +5,10 @@ import { MaterialModule } from 'src/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
-  declarations: [NavbarComponent, HomepageComponent, FooterComponent],
+  declarations: [NavbarComponent, HomepageComponent, FooterComponent, BaseComponent],
   imports: [CommonModule, MaterialModule],
   exports: [NavbarComponent, HomepageComponent, FooterComponent],
 })
