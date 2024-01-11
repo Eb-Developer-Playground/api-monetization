84efa8f62c40698354a0edfb8b452c1e5da40995
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-11 14:28:49 +0300

    update nav to account for when a user is logged in and out

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @Input() userLoggedOut = false;
 
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/auth/login']);
+  }
+
+  signUp() {
+    this.router.navigate(['/auth/signup']);
+  }
+
+  accountPage() {
+    this.router.navigate(['/account']);
+  }
 }
