d3cbc6cc2a4f10117c831d3d94eeea51e72453e9
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-12 10:01:44 +0300

    import reactive forms

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -5,9 +5,15 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { MaterialModule } from 'src/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent],
-  imports: [CommonModule, AuthenticationRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthenticationModule {}
