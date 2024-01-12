56a4924c108cae4368afc31eb675f19ca39e7e04
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-12 10:03:22 +0300

    updateed path to portal

--- src/app/portal/portal-routing.module.ts
+++ src/app/portal/portal-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortalComponent } from './components/base/portal.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ApiDocumentationComponent } from './components/api-documentation/api-documentation.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PortalComponent,
+    // redirectTo: '/portal/dash',
+    // pathMatch: 'full',
     children: [
       {
-        path: 'dash',
+        path: '',
         component: DashboardComponent,
+      },
+      {
+        path: 'documentation/:id',
+        component: ApiDocumentationComponent,
       },
     ],
   },
--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -5,9 +5,11 @@ import { PortalRoutingModule } from './portal-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PortalComponent } from './components/base/portal.component';
 import { LayoutModule } from '../layout/layout.module';
+import { MaterialModule } from 'src/material.module';
+import { ApiDocumentationComponent } from './components/api-documentation/api-documentation.component';
 
 @NgModule({
-  declarations: [DashboardComponent, PortalComponent],
-  imports: [CommonModule, PortalRoutingModule, LayoutModule],
+  declarations: [DashboardComponent, PortalComponent, ApiDocumentationComponent],
+  imports: [CommonModule, PortalRoutingModule, LayoutModule, MaterialModule],
 })
 export class PortalModule {}
