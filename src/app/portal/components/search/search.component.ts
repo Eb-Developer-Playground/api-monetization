efe48995e2f36ccd161cacaadb006d751d15b3ac
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-13 10:02:24 +0300

    added a search component

---
+++ src/app/portal/components/search/search.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { FormControl } from '@angular/forms';
+import { startWith } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent implements OnInit {
+  cardData!: any[];
+  filteredCardData!: any[];
+
+  formCtrl = new FormControl();
+  formFilterCtrl = new FormControl();
+
+  constructor(private dataService: DataService) {}
+  ngOnInit(): void {
+    this.cardData = this.dataService.cardData;
+    this.formFilterCtrl.valueChanges.pipe(startWith('')).subscribe((value) => {
+      console.log('Search input value:', this.formFilterCtrl.value);
+      this.filteredCardData = this.filterCardData(value);
+    });
+  }
+
+  private filterCardData(value: string): any[] {
+    const filterValue = value.toLowerCase();
+    return this.cardData.filter(
+      (data) =>
+        data.title.toLowerCase().includes(filterValue) ||
+        data.content.toLowerCase().includes(filterValue)
+    );
+  }
+}
