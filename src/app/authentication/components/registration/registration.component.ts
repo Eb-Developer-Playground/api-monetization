673e1d519d6036b4c5a4b28811499a99522274a8
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-24 16:59:41 +0300

    validated and added visibility toggle on registration form

--- src/app/authentication/components/registration/registration.component.ts
+++ src/app/authentication/components/registration/registration.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserSignUp } from '../../interfaces/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss'],
 })
-export class RegistrationComponent {
-  constructor(private router: Router) {}
+export class RegistrationComponent implements OnInit {
+  registerForm!: FormGroup;
+  passwordHidden: Boolean = true;
+  confirmPasswordHidden: Boolean = true;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
 
   register() {
-    this.router.navigate(['/portal']);
+    if (this.registerForm.valid) {
+      this.router.navigate(['/portal']);
+      this.registerForm.reset();
+    }
+  }
+
+  togglePasswordVisibility(field: string) {
+    if (field === 'password') {
+      this.passwordHidden = !this.passwordHidden;
+    } else if (field === 'confirmPassword') {
+      this.confirmPasswordHidden = !this.confirmPasswordHidden;
+    }
   }
 }
