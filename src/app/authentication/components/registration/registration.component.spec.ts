4783c5409833695f8d76de8d39d8574917630d6f
Sam Nujoma <sam.otieno@equitybank.co.ke>
2024-01-25 09:19:25 +0300

    import reactive form module in test file

--- src/app/authentication/components/registration/registration.component.spec.ts
+++ src/app/authentication/components/registration/registration.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegistrationComponent } from './registration.component';
 import { MaterialModule } from 'src/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
@@ -11,7 +12,7 @@ describe('RegistrationComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RegistrationComponent],
-      imports: [MaterialModule, BrowserAnimationsModule],
+      imports: [MaterialModule, BrowserAnimationsModule, ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
